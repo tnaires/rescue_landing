54ab95866cf08b050c2a8112995a86774a29792e
Tarso Aires <tnaires@gmail.com>
2016-06-29 02:05:11 -0300

    Reenable stages

--- js/objects/levels/level_chain.js
+++ js/objects/levels/level_chain.js
@@ -1,12 +1,8 @@
 Level.TITLE.setNextLevel(Level.ONE);
 Level.ONE.setNextLevel(Level.TWO);
-Level.TWO.setNextLevel(Level.END);
+Level.TWO.setNextLevel(Level.THREE);
+Level.THREE.setNextLevel(Level.FOUR);
+Level.FOUR.setNextLevel(Level.FIVE);
+Level.FIVE.setNextLevel(Level.SIX);
+Level.SIX.setNextLevel(Level.END);
 Level.END.setNextLevel(Level.TITLE);
-
-// Level.TITLE.setNextLevel(Level.ONE);
-// Level.ONE.setNextLevel(Level.TWO);
-// Level.TWO.setNextLevel(Level.THREE);
-// Level.THREE.setNextLevel(Level.FOUR);
-// Level.FOUR.setNextLevel(Level.FIVE);
-// Level.FIVE.setNextLevel(Level.SIX);
-// Level.SIX.setNextLevel(Level.TITLE);
