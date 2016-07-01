05a6752afe55aa1045b2c09b5005d55ca42c7450
Tarso Aires <tnaires@gmail.com>
2016-07-01 00:49:45 -0300

    Set different colors to fuel bar depending on remaining fuel

--- js/objects/fuel_bar.js
+++ js/objects/fuel_bar.js
@@ -1,7 +1,12 @@
 var FuelBar = function(_lander) {
   var
+    GREEN = '#00FF00',
+    YELLOW = '#FFFF00',
+    ORANGE = '#FF9900',
+    RED = '#FF0000',
+
     _drawFuelRect = function(context) {
-      context.strokeStyle = '#FFFF00';
+      context.strokeStyle = YELLOW;
       context.lineWidth = 4;
       context.strokeRect(500, 4, 108, 20);
     },
@@ -9,7 +14,16 @@ var FuelBar = function(_lander) {
     _drawFuelFilling = function(context) {
       var fuelPercentage = Math.floor(_lander.currentFuel() / _lander.currentLevel().fuel() * 100);
 
-      context.fillStyle = '#00FF00';
+      if (fuelPercentage < 25) {
+        context.fillStyle = RED;
+      } else if (fuelPercentage < 50) {
+        context.fillStyle = ORANGE;
+      } else if (fuelPercentage < 75) {
+        context.fillStyle = YELLOW;
+      } else {
+        context.fillStyle = GREEN;
+      }
+
       context.fillRect(504, 8, fuelPercentage, 12);
     },
 
