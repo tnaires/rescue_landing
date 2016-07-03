ffa8ddfa2478272204bca0f89aeea3cf7bf3cca1
Tarso Aires <tnaires@gmail.com>
2016-07-03 00:53:21 -0300

    Change title and end screens

--- js/objects/levels/end.js
+++ js/objects/levels/end.js
@@ -1,11 +1,25 @@
 Level.END = new Info(function(context) {
   var canvas = context.canvas;
+
+  context.fillStyle = '#FFFFCC';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
   context.textAlign = 'center';
   context.fillStyle = '#000000';
-
-  context.font = 'bold 100px "Lucida Console", Monaco, monospace';
+  context.font = 'bold 80px "Lucida Console", Monaco, monospace';
   context.fillText('CONGRATZ!', canvas.width / 2, 120);
 
   context.font = '20px "Lucida Console", Monaco, monospace';
-  context.fillText('Thanks for playing!', canvas.width / 2, 400);
+  context.fillText('Developed and designed by', canvas.width / 2, 250);
+
+  context.font = 'bold 40px "Lucida Console", Monaco, monospace';
+  context.fillText('Tarso', canvas.width / 2, 310);
+
+  context.fillStyle = '#0066FF';
+  context.font = '20px "Lucida Console", Monaco, monospace';
+  context.fillText('tnaires@gmail.com', canvas.width / 2, 340);
+
+  context.fillStyle = '#000000';
+  context.font = '20px "Lucida Console", Monaco, monospace';
+  context.fillText('Thanks for playing!', canvas.width / 2, 430);
 });
--- js/objects/levels/title.js
+++ js/objects/levels/title.js
@@ -1,12 +1,16 @@
 Level.TITLE = new Info(function(context) {
   var canvas = context.canvas;
   context.textAlign = 'center';
-  context.fillStyle = '#000000';
 
+  context.fillStyle = '#000000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = '#FFFF00';
   context.font = 'bold 100px "Lucida Console", Monaco, monospace';
   context.fillText('Rescue', canvas.width / 2, 120);
   context.fillText('Landing!', canvas.width / 2, 240);
 
+  context.fillStyle = '#FFFFFF';
   context.font = '20px "Lucida Console", Monaco, monospace';
   context.fillText('Press SPACE to begin', canvas.width / 2, 400);
 });
